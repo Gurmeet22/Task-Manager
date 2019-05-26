44142b0452d9495c75ec511792ca4873eacba00e
Gurmeet22 <gsinghs1998@gmail.com>
2019-05-26 18:05:00 +0530

    endpoints added

--- src/index.js
+++ src/index.js
@@ -26,6 +26,46 @@ app.post('/tasks', (req, res) => {
     })
 })
 
+app.get('/users', (req, res) => {
+    User.find({}).then((users) => {
+        res.send(users)
+    }).catch((e) => {
+        res.status(500).send()
+    })
+})
+
+app.get('/users/:id', (req, res) => {
+    const _id = req.params.id
+    User.findById(_id).then((user) => {
+        if(!user){
+            return res.status(404).send()
+        }
+        res.send(user)
+    }).catch((e) => {
+        res.status(500).send()
+    })
+})
+
+app.get('/tasks', (req, res) => {
+    Task.find({}).then((tasks) => {
+        res.send(tasks)
+    }).catch((e) => {
+        res.status(500).send()
+    })
+})
+
+app.get('/tasks/:id', (req, res) => {
+    const _id = req.params.id
+    Task.findById(_id).then((task) => {
+        if(!task){
+            return res.status(404).send('Not found')
+        }
+        res.send(task)
+    }).catch((e) => {
+        res.status(500).send()
+    })
+})
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
 })
